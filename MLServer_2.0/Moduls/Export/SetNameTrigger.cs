20c9f838ef7b2c63f3331d15625fc502fe7d23a7
AlexLan73 <diving_73@mail.ru>
2021-04-19 19:00:38 +0300

    Refactoring

--- MLServer_2.0/Moduls/Export/SetNameTrigger.cs
+++ MLServer_2.0/Moduls/Export/SetNameTrigger.cs
@@ -12,7 +12,6 @@ using System.Threading.Tasks;
 using TypeDStringMemoryInfo = System.Collections.Concurrent.ConcurrentDictionary<string,
         System.Collections.Concurrent.ConcurrentDictionary<string, MLServer_2._0.Moduls.ClfFileType.MemoryInfo>>;
 
-//public ConcurrentDictionary<string, ConcurrentDictionary<string, MemoryInfo>> DbConfig;
 
 namespace MLServer_2._0.Moduls.Export
 {
@@ -22,10 +21,10 @@ namespace MLServer_2._0.Moduls.Export
         private Config0 _config;
         private readonly ILogger _ilogger;
         public Task RunTask = null;
-        public string _patternFile;
-        private string _typeconvert;
-        private string _ext;
-        private string _pathConvert;
+        private readonly string _patternFile;
+        private readonly string _typeconvert;
+        private readonly string _ext;
+        private readonly string _pathConvert;
         private ConcurrentDictionary<string, string> _mem1 = new ConcurrentDictionary<string, string>();
 
         #endregion
@@ -41,59 +40,53 @@ namespace MLServer_2._0.Moduls.Export
             var _x0 = _config.DbConfig.Where(x => x.Key.ToLower().Contains("_m2_")).Select(y=> y.Value);//
             foreach (var item in _x0)
             {
-                foreach (var item1 in item)
+                foreach (var (key, value) in item)
                 {
-                    string s0 = _numTrigger(item1.Value);
+                    var s0 = _numTrigger(value);
                     var ww = s0.Length >0? "_Trigger"+ s0 : "";
-                    _mem1.AddOrUpdate(item1.Key, ww, (_,_) => ww);
+                    _mem1.AddOrUpdate(key, ww, (_,_) => ww);
                 }
             }
         }
 
         private string _numTrigger(MemoryInfo meminfo)
         {
-            string s = "_(x)";
+            var s = "_(x)";
             var zx= meminfo.TriggerInfo.Select(x => x.Trigger.Split(" ")[1]).ToList().Distinct().ToArray(); ;
-            string ss0 = "";
-            foreach (var item in zx)
-                ss0 += s.Replace("x", item);
-            return ss0;
+            return zx.Aggregate("", (current, item) => current + s.Replace("x", item));
         }
         private List<string> _findFileDirClf() => Directory.GetFiles(_pathConvert, "*"+_ext)
                 .Where(x => Regex.Matches(x, _patternFile, RegexOptions.IgnoreCase).Count == 1)
-                .Select(z => Path.GetFileName(z))
+                .Select(Path.GetFileName)
                 .ToList();
 
-        private async Task _rename_m1(List<string> ls)
-        {
-            await Task.Run(() =>
-              {
-                  var _ls = new List<string>(ls);
-                  foreach (var item in _ls)
-                  {
-                      string _file = Path.GetFileName(item);
-
-                      _file = _file.ToUpper().Replace(")F", ")_F");
-
-                      var t = _pathConvert + _file;
-                  }
-              });
-        }
-        private async Task _rename_m2(List<string> ls)
+        private async Task _rename_m1(IReadOnlyCollection<string> ls1)
         {
             await Task.Run(() =>
             {
-                List<string> _ls = new List<string>(ls);
-                foreach (var item in _ls)
+                var ls = new List<string>(ls1);
+                foreach (var t in from item in ls 
+                                select Path.GetFileName(item) into file 
+                                select file.ToUpper().Replace(")F", ")_F") into _file 
+                                select _pathConvert + _file)
                 {
-                    string _file = Path.GetFileName(item).ToUpper();
-
-                    int i = _file.IndexOf(")F");
-                    var _file0 = _file.Substring(0, i+1)+"_";
-                    var _file1 = _file.Substring(i+1).Split(".");
-                    string _fnum = _mem1[_file1[0]];
-                    string s0 = _file0 + _file1[0] + _fnum +"."+ _file1[1];
-                    var f0 = _pathConvert + item;
+                }
+            });
+        }
+        private async Task _rename_m2(List<string> ls1)
+        {
+            await Task.Run(() =>
+            {
+                List<string> _ls = new List<string>(ls1);
+                foreach (var f0 in from item in _ls 
+                                    let file = Path.GetFileName(item).ToUpper() 
+                                    let i = file.IndexOf(")F", StringComparison.Ordinal) 
+                                    let file0 = file.Substring(0, i+1)+"_" 
+                                    let file1 = file.Substring(i+1).Split(".") 
+                                    let fnum = _mem1[file1[0]] 
+                                    let s0 = file0 + file1[0] + fnum +"."+ file1[1] 
+                                    select _pathConvert + item)
+                {
                 }
             });
         }
